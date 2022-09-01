f97c606bba0efa68c50fda4df5cfa7937f90b9e1
owaisali853 <110772807+owaisali853@users.noreply.github.com>
2022-09-01 18:09:46 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,115 @@
+// alert("hello world");
+
+// var a = 5;
+// var b = 10;
+// var c = a+b;
+// alert(c);
+
+// var a = 5;
+// var b = 10;
+// var c = a+b;
+// console.log(c);
+
+// var x = 5;
+// var y = "owais";
+// var z = y+x;
+// alert(z);
+
+// var a = 5;
+// var b = "10";
+// var c = a*b;
+// alert(c);
+
+
+// var a = 3;
+// var b = 5;
+// var c = 7;
+// var d = b - a * c;
+// alert(d);
+
+// var a = 5;
+// var b = a++;
+// alert(b);
+
+// var a = 5;
+// var b = ++a;
+// alert(b);
+
+// var x = 10;
+// var y = x++ + ++x - x + x++ - ++x +x;
+//         // 10  + 12  - 12 + 12 - 14 + 14 =22
+// var z = y;
+// alert(z)
+
+// var a = +prompt("Enter value","Your value is");
+// var b = a + 10;
+// alert(b)
+
+// var a = +prompt("Enter value","Your value is");
+// var b = a + 10;
+// console.log("The outpot is", + b)
+
+// var a = +prompt("Enter value","Your value is");
+// var b = a + 10;
+// document.write("The output is" + b)
+
+// var age = +prompt("Enter your age");
+// if (age == 18){
+//         alert("you are 18")
+// }
+// else if(age < 18) {
+//         alert("you are under 18")
+
+// }
+// else{
+//         alert("You are 18+")
+// }
+
+// var age = +prompt("Enter your age")
+// var gender = prompt("Enter your gender")
+// if (age > 18 && gender == "male"){
+//         alert("you are allow to ride")
+// }
+// else{
+//         alert("you are not allowed")
+// }
+
+// var age = +prompt("Enter your age")
+// var gender = prompt("Enter your gender")
+// if (age > 18 || gender == "male"){
+//         alert("you are allow to ride")
+// }
+// else{
+//         alert("you are not allowed")
+// }
+
+// var students = ["owais","anas","maaz"]
+// console.log(students[0])
+// alert("wellcome" + students[2])
+
+// var nam = [];
+// nam[0] = "duaa"
+// nam [1] = "owais"
+// alert(nam);
+
+// last index remove
+// var students = ["owais","anas","maaz"]
+// students.pop()
+// students.push("daud")
+// alert(students)
+
+// first index remove and add
+
+// var students = ["owais","anas","maaz"]
+// students.shift()
+// students.unshift("irtiza")
+// console.log(students)
+
+// cut
+// var students = ["owais","duaa","irtiza"]
+// students.splice(0,1,"owaisali")
+// console.log(students)
+
+// copy
+var students = ["owais","duaa","irtiza"]
+
